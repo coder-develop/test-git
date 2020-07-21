696b55683d511c86eafa425a91b5660edfc6b3ff
赵涛 <http://132.46.110.79:8099/zhaotao/apoll-otest.git>
2020-07-21 15:22:45 +0800

    初探github

---
+++ src/main/java/com/tao/test/HelloTest.java
@@ -0,0 +1,14 @@
+package com.tao.test;
+
+/**
+ * @Package: com.tao.test
+ * @Description:
+ * @author: 赵涛
+ * @date: 2020/7/21 15:20
+ */
+public class HelloTest {
+
+    public static void main(String[] args) {
+        System.out.println("第一个Git上传");
+    }
+}
